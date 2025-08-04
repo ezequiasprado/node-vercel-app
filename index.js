a50249738b10ee1983ca13eba15c65b01415c99e
Ezequias Moreira <ezequias.moreira.do.prado@gmail.com>
2025-08-03 23:16:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Vercel!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
